22324de4e688e879be0f37faa05d8397547591c7
Amarjit-pheiroijam <spartanjaggerz@gmail.com>
2021-11-04 00:19:16 +0530

    Build Fail Fix

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,5 @@ module.exports = {
     'Cascading Style Sheet': ['css1', 'css2', 'css3', 'css4', 'css5'],
     'Javascript':['js1','js2','js3','js4','js5','js6','js7','js8','js9','js10'],
     'React': ['React1', 'React2', 'React3', 'React4', 'React5', 'React6','React7','React8'],
-  
-    
   },
 };
